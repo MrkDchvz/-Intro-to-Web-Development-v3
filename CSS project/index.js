315c45762aa4c0a2eb1bc5cdfc459f64bbdd0bf7
Markufuu <madechavez22@gmail.com>
2023-01-11 11:44:30 +0800

    finished css project

--- CSS project/index.js
+++
@@ -1,7 +0,0 @@
-const button = document.querySelector('.nav__button');
-const nav = document.querySelector('.nav__list');
-
-button.addEventListener('click', () => {
-  button.classList.toggle('toggled');
-  nav.classList.toggle('toggled');
-})
